580fa5a7e109b6aaac898612e8fd6020301645d2
Vitasic <vitasic-pokataev@yandex.ru>
2012-08-20 09:29:52 +0400

    Core/Scripts Tranmogrifications

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -23,6 +23,8 @@ void AddSC_example_escort();
 void AddSC_example_gossip_codebox();
 void AddSC_example_misc();
 void AddSC_example_commandscript();
+// Transmogrify
+void AddSC_transmogrify_script();
 
 //TeleNPC2
 void AddSC_npc_teleport();
@@ -1284,6 +1286,7 @@ void AddBattlegroundScripts()
 
 void AddCustomScripts()
 {
+    AddSC_transmogrify_script();
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
     //TeleNPC2
